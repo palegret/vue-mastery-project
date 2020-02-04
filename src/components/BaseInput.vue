eb78a2304cc0ed6613c332e10839c2a5a2d423df
Philip Alegret <palegret@procentrix.com>
2020-02-04 11:02:16 -0500

    Code for lesson 7, 'Reusable Form Components: BaseInput'.

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,10 +1,31 @@
 <template>
-<div>Input</div>
+    <div>
+      <label v-if="label">{{ label }}</label>
+      <input :value="value" v-bind="$attrs" @input="onInput" />
+    </div>
 </template>
 
 <script>
 export default {
+  // So attributes that are passed in don't automatically go on the root element 
+  // (<div>), but whichever element has the `v-bind="$attrs"` attribute on it.
+  // However:
+  // "...inheritAttrs: false...DOES NOT affect style and class bindings."
+  // See: https://vuejs.org/v2/guide/components-props.html
+  inheritAttrs: false, 
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    value: [String, Number]
+  },
   data: () => ({
-  })
+  }),
+  methods: {
+    onInput(event) {
+      this.$emit('input', event.target.value)
+    }
+  }
 }
 </script>
