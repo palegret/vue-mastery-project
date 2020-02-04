92a4b095e86200b65cf188bf74afafa0b1124947
Philip Alegret <palegret@procentrix.com>
2020-02-04 11:05:22 -0500

    Code for lesson 9, 'Reusable Form Components: BaseButton'.

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,10 +1,69 @@
 <template>
-<div>Button</div>
+  <button v-bind="$attrs" v-on="$listeners" class="button" :class="buttonClass">
+    <slot/> 
+  </button>
 </template>
 
 <script>
-export default {
+export default { //
+  inheritAttrs: false, 
+  props: {
+    buttonClass: String
+  },
   data: () => ({
   })
 }
 </script>
+
+<style lang="stylus" scoped>
+.button
+  display inline-flex
+  align-items center
+  justify-content space-between
+  height 52px
+  padding 0 40px
+  background transparent
+  border none
+  border-radius 6px
+  text-align center
+  font-weight 600
+  white-space nowrap
+  transition all 0.2s linear
+  &:hover
+    -webkit-transform scale(1.02)
+    transform scale(1.02)
+    box-shadow 0 7px 17px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)
+  &:active
+    -webkit-transform scale(1)
+    transform scale(1)
+    box-shadow none
+  &:focus
+    outline 0
+  &.-fill-gradient
+    background linear-gradient(to right, #16c0b0, #84cf6a)
+    color #ffffff
+  &:disabled
+    -webkit-transform scale(1)
+    transform scale(1)
+    box-shadow none
+    background #eeeeee
+  & + .button
+    margin-left 1em
+  &.-fill-gray
+    background rgba(0, 0, 0, 0.5)
+    color #ffffff
+  &.-size-small
+    height 32px
+  &.-icon-right
+    text-align left
+    padding 0 20px
+    & > .icon
+      margin-left 10px
+  &.-icon-left
+    text-align right
+    padding 0 20px
+    & > .icon
+      margin-right 10px
+  &.-icon-center
+    padding 0 20px
+</style>
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -3,42 +3,26 @@
     <h1>Create Event</h1>
     <small>{{event.user}}</small>
      <form @submit.prevent="createEvent">
-      <label>Select a category</label>
-      <select v-model="event.category">
-        <option v-for="cat in sortedCategories" :key="cat.value" :value="cat.value">{{ cat.label }}</option>
-      </select>
+      <h3>What type of event is this?</h3>
+      <BaseSelect v-model="event.category" :options="sortedCategories" label="Category" />
+
       <h3>Name & describe your event</h3>
-      <div class="field">
-        <label>Title</label>
-        <input v-model="event.title" type="text" placeholder="Add an event title"/>
-      </div>
-      <div class="field">
-        <label>Description</label>
-        <input v-model="event.description" type="text" placeholder="Add a description"/>
-      </div>
+      <BaseInput v-model="event.title" type="text" class="field" label="Title" placeholder="Add an event title" />
+      <BaseInput v-model="event.description" type="text" class="field" label="Description" placeholder="Add a description" />
+
       <h3>Where is your event?</h3>
-      <div class="field">
-        <label>Location</label>
-        <input v-model="event.location" type="text" placeholder="Add a location"/>
-      </div>
+      <BaseInput v-model="event.location" type="text" class="field" label="Location" placeholder="Add a location" />
+
       <h3>When is your event?</h3>
       <div class="field">
         <label>Date</label>
         <datepicker v-model="event.date" placeholder="Select a date"/>
       </div>
-      <div class="field">
-        <label>Select a time</label>
-        <select v-model="event.time">
-          <option value="">Select a time</option>
-          <optgroup label="Morning Times">
-            <option v-for="time in amTimes" :key="`am_${time.value}`" :value="time.value">{{ time.label }}</option>
-          </optgroup>
-          <optgroup label="Afternoon Times">
-            <option v-for="time in pmTimes" :key="`pm_${time.value}`" :value="time.value">{{ time.label }}</option>
-          </optgroup>
-        </select>
-      </div>
+
+      <TimeSelect v-model="event.time" :startTime="8" :endTime="17" label="Time" class="field" /> 
+
       <div class="buttons">
+        <BaseButton type="submit" :disabled="!formValid" buttonClass="-fill-gradient">Submit</BaseButton>        
         <input type="submit" :disabled="!formValid" class="button -fill-gradient" value="Submit"/>
       </div>
     </form>    
@@ -54,9 +38,12 @@ import NProgress from 'nprogress'
 
 import Datepicker from 'vuejs-datepicker'
 
+import TimeSelect from '@/components/TimeSelect'
+
 export default {
   components: {
-    Datepicker
+    Datepicker,
+    TimeSelect
   },
   data() {
     return {
@@ -86,31 +73,6 @@ export default {
   computed: {
     catLength() {
       return this.$store.getters.catLength;
-    },
-    times() {
-      const timesArray = [{value: '', label: 'Select a time'}]
-
-      for (let hour = 0; hour <= 24; hour++) {
-        let hourLabel;
-
-        if (hour === 0) {
-          hourLabel = '12:00 AM'
-        } else if (hour < 13) {
-          hourLabel = (hour < 10 ? `0${hour}:00` : `${hour}:00`) + ' AM'
-        } else {
-          hourLabel = (hour - 12 < 10 ? `0${hour - 12}:00` : `${hour - 12}:00`) + ' PM'
-        }
-
-        timesArray.push({value: hour, label: hourLabel})
-      }
-
-      return timesArray
-    },
-    amTimes() {
-      return this.times.filter(time => time.value > 7 && time.value < 12)
-    },
-    pmTimes() {
-      return this.times.filter(time => time.value > 12 && time.value < 18)
     },
     sortedCategories() {
       const sorted = [].concat(this.categories).sort().map(c => ({value: c, label: c}))
